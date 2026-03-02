faa7ca76b169640babd35047d1c9e86ae5fdf096
JourneyToMasteryFromContributor <dburnsidework@gmail.com>
2026-03-02 19:41:57 +0000

    Add files via upload
    Similar university task

--- StringAnalyzer.java
+++ StringAnalyzer.java
@@ -1,49 +1,49 @@
-import java.util.Scanner;
+String/*
+ * This is an example solution for the String Analyser program which forms the first
+ * Practical Skills Assessment on COM102.
+ *
+ * @author COM102
+ * Date created: Feb 2024
+ * Version 1.0
+ */
+package com102;
 
-public class StringAnalyzer {
+import java.util.Scanner ;
 
-    private char[] letters = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+
+public class StringAnalyzer{
+	
+    //Array for matching letters as string is analysed
+	private char[] letters = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'} ;
+    //array that holds the frequency of occurences of each letter in its respictive index positon 'a'=0, 'z'=25
     private int[] frequency = new int[26];
-
-    private Scanner scan;
-    private String input;
-
-    public static void main(String[] args) {
-        StringAnalyzer myAnalyzer = new StringAnalyzer();
-
-        myAnalyzer.start();
+	
+	
+    
+    public static void main(String[] args){
+        // Instantiating self class so we can make use of the instance variables declared herewith and
+        // avoid the need to make more methods in this class static.
+            
+        StringAnalyzer myAnalyzer = new StringAnalyzer() ;
+        
+        myAnalyzer.start() ;
     }
-
-    private void start() {
-        getString();
-        analyzeString();
-        reportAnalysis();
-
-        System.out.println("Goodbye");
+    
+    private void start(){
+        // Decomposing the problem into a discrete set of well focussed steps.
+            
+        this.getString() ;
+        this.analyzeString() ;
+        this.reportAnalysis() ;  
+                    
+        System.out.println("Program Terminating, Goodbye");
     }
-
-    // Get string from user
-    private void getString() {
-        scan = new Scanner(System.in);
-        System.out.println("Please enter a string of text for analysis:");
-        input = scan.nextLine().toLowerCase();
-    }
-
-    private void analyzeString() {
-        for (int i = 0; i < input.length(); i++) {
-            char ch = input.charAt(i);
-
-            if (ch >= 'a' && ch <= 'z') { // Check if letter is valid
-                frequency[ch - 'a']++;
-            }
-        }
-    }
-
-    private void reportAnalysis() {
-        System.out.println("\nLetter Analysis:");
-
-        for (int i = 0; i < letters.length; i++) {
-            System.out.println(letters[i] + ": " + frequency[i]);
-        }
-    }
-}
+	//
+	//Your Task - Complete thje following 3 methods as instructed in the spec.
+	//1. getString() - Read a string of text from keyboard
+	//2. encryptString() - Perform encryption on the strinig as required
+	//3. reportAnalysis() - Print the results back to the user on the screen 
+	// Feel free to declare other variables as needed.
+	
+    
+} // End of class StringAnalyser
